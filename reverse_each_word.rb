a36fcd55b2e3098098750083837722d243b3b370
Annie Wiegel <annewiegel17@gmail.com>
2021-02-04 02:44:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  array.collect do |word|
-    word.reverse
+  reversed = []
+  reversed = array.collect {|word| word.reverse}
+  array.join(" ")
   end
 end
